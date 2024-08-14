e3d8140b77982fba187ce7681e6b9a166c65bb49
Sebastian Espinoza Wong <sebasesp02@gmail.com>
2024-08-13 23:57:49 -0400

    commit

--- src/metrics_calculation.py
+++ src/metrics_calculation.py
@@ -48,7 +48,35 @@ def calculate_metrics(model_pred_df, genre_list, genre_true_counts, genre_tp_cou
         fp = genre_fp_counts[genre]
         fn = genre_true_counts[genre] - tp
     
+        if tp + fp > 0:
+            precision = tp / (tp + fp)
+        else:
+            precision = 0
+        
+        if tp + fn > 0:
+            recall = tp / (tp + fn)
+        else:
+            recall = 0
+        
+        if precision + recall > 0:
+            f1 = 2 * (precision * recall) / (precision + recall)
+        else:
+            f1 = 0
+        
+        macro_prec_list.append(precision)
+        macro_recall_list.append(recall)
+        macro_f1_list.append(f1)
+        
+        micro_tp += tp
+        micro_fp += fp
+        micro_fn += fn
     
+    # These are the micrometrics.
+    micro_precision = micro_tp / (micro_tp + micro_fp) if (micro_tp + micro_fp) > 0 else 0
+    micro_recall = micro_tp / (micro_tp + micro_fn) if (micro_tp + micro_fn) > 0 else 0
+    micro_f1 = 2 * (micro_precision * micro_recall) / (micro_precision + micro_recall) if (micro_precision + micro_recall) > 0 else 0
+    
+    return micro_precision, micro_recall, micro_f1, macro_prec_list, macro_recall_list, macro_f1_list
    
 
     
@@ -72,4 +100,22 @@ def calculate_sklearn_metrics(model_pred_df, genre_list):
     true_matrix = pd.DataFrame(true_rows)
     '''
 
-    # Your code here
+    from sklearn.metrics import precision_recall_fscore_support
+
+    #preparing the true and predicted labels 
+    pred_rows = []
+    true_rows = []
+
+    for index, row in model_pred_df.itterws():
+        actual_genres = eval(row['actual genres'])
+        predicted_genre = row['predicted']
+        pred_rows.append(predicted_genre)
+        true_rows.extend(actual_genres)
+
+    true_rows = [genre_list.index(genre) for genre in true_rows]
+    pred_rows = [genre_list.index(genre) for genre in pred_rows]
+
+    macro_prec, macro_rec, macro_f1, _=precision_recall_fscore_support(true_rows, pred_rows, average='macro', zero_division=0)
+    micro_prec, micro_rec, micro_f1, _ = precision_recall_fscore_support(true_rows, pred_rows, average='micro', zero_division=0)
+
+    return macro_prec, macro_rec, macro_f1, micro_prec, micro_rec, micro_f1
